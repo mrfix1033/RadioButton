d0ad4e932401ba0033199e61f21d1b16b7df9251
Сергей Осипов <mrfix1033@mail.ru>
2024-11-01 08:09:19 +0300

    Now the answer is accepted immediately after the selection

--- app/src/main/java/ru/mrfix1033/radiobutton/QuestionActivityAbstract.kt
+++ app/src/main/java/ru/mrfix1033/radiobutton/QuestionActivityAbstract.kt
@@ -1,14 +1,9 @@
 package ru.mrfix1033.radiobutton
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RadioButton
 import android.widget.RadioGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 
 abstract class QuestionActivityAbstract(
     val correctAnswerCheckedId: Int,
@@ -17,9 +12,6 @@ abstract class QuestionActivityAbstract(
 ) : AppCompatActivity() {
 
     private lateinit var radioGroup: RadioGroup
-    private lateinit var textViewState: TextView
-    private lateinit var buttonGetAnswerOrNext: Button
-    private var buttonIsClicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,31 +20,13 @@ abstract class QuestionActivityAbstract(
         var score = intent.getIntExtra("score", 0)
 
         radioGroup = findViewById(R.id.radioGroup)
-        textViewState = findViewById(R.id.textViewState)
-        buttonGetAnswerOrNext = findViewById(R.id.buttonGetAnswerOrNext)
 
         radioGroup.setOnCheckedChangeListener { radioGroup, i ->
-            buttonGetAnswerOrNext.isVisible = true
-        }
-
-        buttonGetAnswerOrNext.setOnClickListener {
-            if (buttonIsClicked) {
-                val intent = Intent(this, nextActivityJavaClass)
-                intent.putExtra("score", score)
-                startActivity(intent)
-            } else {
-                if (radioGroup.checkedRadioButtonId == correctAnswerCheckedId) {
-                    textViewState.text = getString(R.string.correct)
-                    textViewState.setTextColor(Color.GREEN)
-                    score += 100
-                } else {
-                    textViewState.text =
-                        getString(R.string.incorrect).format(findViewById<RadioButton>(correctAnswerCheckedId)!!.text)
-                    textViewState.setTextColor(Color.RED)
-                }
-                buttonIsClicked = true
-                buttonGetAnswerOrNext.setText(R.string.next_question)
-            }
+            if (radioGroup.checkedRadioButtonId == correctAnswerCheckedId)
+                score += 100
+            val intent = Intent(this, nextActivityJavaClass)
+            intent.putExtra("score", score)
+            startActivity(intent)
         }
     }
 }
